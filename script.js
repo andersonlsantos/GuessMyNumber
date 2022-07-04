6d39ce817c3d66a0031b5324c02036b65c5ce2ff
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-04 13:41:47 -0300

    controlRand

--- script.js
+++ script.js
@@ -10,14 +10,31 @@ const selAll = (idClass) => {
     return elem
 }
 
-const rand = () => parseInt(Math.random() * 21)
+let rand = () => {
+    let lista = []
+
+    for(let som = 1; som <= 20; som++){
+        lista.push(som)
+    }
+    
+    let randi = Math.round(Number(Math.random() * 19))
+
+    return lista[randi]
+}
 
 let guess = sel('.guess')
 
-let b1 = sel('#b1').onclick = () => {
-    if(guess.value == rand()) {
-        console.log(`Acertou, ${guess.value} == ${rand()}`)
+function controlRand(){
+    let num = Number(guess.value)
+
+    // console.log(rand(), num)
+
+    if(rand() === num) {
+        console.log(`Acertou, ${num} == ${rand()}`)
     }else{
-        console.log(`Errou, ${guess.value} != ${rand()}`)
+        console.log(`Errou, ${num} != ${rand()}`)
     }
 }
+
+let b1 = sel('#b1').onclick = controlRand
+
