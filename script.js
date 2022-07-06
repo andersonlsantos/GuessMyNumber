907d12619dfdfdefcafd1e6bcc9ca0b4ac11b2a4
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-06 10:32:43 -0300

    score

--- script.js
+++ script.js
@@ -10,11 +10,24 @@ const selAll = (idClass) => {
     return elem
 }
 
-sel('#b1').addEventListener('click', () => {
+let score = 20
+
+const controlClick = () => {
+
+    score--
+
+    let cont = 1
 
     let rand = Math.round(Math.random() * 20)
 
     let guess = Number(sel('.guess').value)
+
+    if(score >= 0) sel('.score').textContent = score.toString()
+    if(score === 0) {
+        sel('#b1').disabled = true
+        sel('.message').textContent = 'Game Over ...'
+        sel('body').style.backgroundColor = 'red'
+    }
 
     if(guess < 1 || guess > 20) {
         sel('.number').textContent = `?`
@@ -22,17 +35,27 @@ sel('#b1').addEventListener('click', () => {
         sel('.number').textContent = `${rand}`
     }
 
-    // console.log(rand, guess)
-
-    if(guess < 1 || guess > 20) {
+    if(guess < 1 || guess > 20 && score !== 0) {
         sel('.message').textContent = 'invalid number !!'
     }else{
         if(rand === guess) {
             sel('.message').textContent = 'You won ....'
+            sel('body').style.backgroundColor = 'green'
+            cont = 0
         }else{
             if(guess > rand) sel('.message').textContent = 'Lower ....'
             if(guess < rand) sel('.message').textContent = 'Higher ....'
+            sel('body').style.backgroundColor = '#222'
         }
     }
-})
+
+    if(cont) {
+        sel('#b1').disabled = false
+    } else {
+        sel('#b1').disabled = true
+    }
+
+}
+
+sel('#b1').addEventListener('click', controlClick)
 
